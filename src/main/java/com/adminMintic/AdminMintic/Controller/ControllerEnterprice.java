67744e8386c99eec793fd5e22cc9b6c8e22ada81
IvanScriptS <iachavez@utp.edu.co>
2022-09-09 15:23:16 -0500

    Until this moment the program is working well and the hos shows what has to show

--- src/main/java/com/adminMintic/AdminMintic/Controller/ControllerEnterprice.java
+++ src/main/java/com/adminMintic/AdminMintic/Controller/ControllerEnterprice.java
@@ -1,9 +1,14 @@
 package com.adminMintic.AdminMintic.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ControllerEnterprice {
 
+    @GetMapping("/")
     public String prueba(){
+
         return "I'm mastering Java Spring Boot and nobody can stop me";
     }
 }
